ccf704d96057e04b1ec204900aaa950d56af88f9
Ayush <ayupithale619@gmail.com>
2026-03-07 00:58:53 +0530

    added consume event for media created delete handler to consumen and deleted given medias

--- media-service/src/eventHandlers/media-event-handlers.js
+++ media-service/src/eventHandlers/media-event-handlers.js
@@ -1,6 +1,31 @@
-const handlePostDeleted = async (event) => {
-  console.log("Post deleted event received", event);
-};
+const logger = require("../utils/logger");
+const { asyncHandler, APIError } = require("../middleware/error-handler");
+const Media = require("../models/media");
+const { deleteMediaCloudinary } = require("../utils/cloudinary");
+
+const handlePostDeleted = asyncHandler(async (event) => {
+  logger.info("Post deleted event received", event);
+
+  const { postId, mediaIds } = event;
+  const mediaToDelete = await Media.find({ _id: { $in: mediaIds } });
+
+  if (!mediaToDelete) {
+    logger.warn("No media found for deletion", mediaIds);
+    return;
+  }
+
+  for (const media of mediaToDelete) {
+    await deleteMediaCloudinary(media.publicId);
+    await Media.findByIdAndDelete(media._id);
+    logger.info(
+      `Media deleted successfully ${media.publicId} assoisted with ${postId}`,
+    );
+  }
+
+  logger.info(
+    `All media deleted successfully ${mediaToDelete.length} assoisted with ${postId}`,
+  );
+});
 
 module.exports = {
   handlePostDeleted,
